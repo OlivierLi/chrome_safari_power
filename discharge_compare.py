90dc34bb6f8f6c02b92e41eef66db617807e42f9
Olivier Li <OlivierLiDupusi@gmail.com>
2021-03-30 11:29:10 -0400

    Make compare script work with discharge

--- discharge_compare.py
+++ discharge_compare.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """
-Use this tool to compare two runs of intel power gadget and find the power use delta.
-
-A "run" consists of N csv reports clearly numbered and located in the same directory.
+Use this tool to compare results from discharge.sh.
 """
 
 import pandas as pd
@@ -12,35 +10,10 @@ import fnmatch
 import shutil
 import argparse
 
-cumulative_energy_column_name = "Cumulative Processor Energy_0(mWh)"
-
-def gather_files(dir_path):
-    if not os.path.isdir(dir_path) or not os.path.exists(dir_path):
-        print("Directory invalid:" + dir_path)
-        sys.exit(-1)
-
-    full_paths = []
-    for dirpath, dirnames, filenames in os.walk(dir_path):
-        for filneame in filenames:
-            full_paths.append(os.path.abspath(os.path.join(dirpath, filneame)))
-
-    # Ignore anything that's not a csv
-    csvs = [ file_path for file_path in full_paths if file_path.endswith(".csv") ]
-
-    return csvs
-
-def load_dir(dir_path):
-
-    agg_data = pd.DataFrame({cumulative_energy_column_name : []})
-
-    for csv in gather_files(dir_path):
-        df = pd.read_csv(csv, engine="python", header=0, skipfooter=15, na_values = [''])
-        cumulative_package_energy = df[cumulative_energy_column_name].max()
-
-        # Not the most efficient thing. Change to concat if this is getting slow.
-        agg_data = agg_data.append({cumulative_energy_column_name: cumulative_package_energy}, ignore_index = True)
-   
-    return clean(agg_data)
+def read_csv(dir_path):
+    df = pd.read_csv(dir_path, engine="python", header=0, skipfooter=0, na_values = [''])
+    df = clean(df[[' Power.Mac.BatteryDischarge']])
+    return df
 
 def get_sign(diff):
     if diff > 0:
@@ -59,7 +32,11 @@ def clean(df):
     df['z_score'] = df.apply(lambda x: (x - x.mean())/x.std())
     df = df[abs(df['z_score']) < 3]
 
-    return df.drop(columns=['z_score'])    
+    cleaned = df.drop(columns=['z_score'])    
+
+    print("Cleaned up :" + str(abs(cleaned.shape[0] - df.shape[0])) + " samples.")
+
+    return cleaned
 
 
 def compare(baseline, experiment):
@@ -69,7 +46,7 @@ def compare(baseline, experiment):
     acceptable_experiment_size_difference = 0.1
     if abs(baseline.shape[0] - experiment.shape[0]) / baseline.shape[0] > 0.1:
         print("The two datasets have mismatched sizes, rerun experiments to get comparable data")
-        return
+        # return
 
     for column in baseline.columns:
         baseline_value = baseline[column].mean()
@@ -88,16 +65,14 @@ def compare(baseline, experiment):
         print("Change in avg {}: {}{:0.2f} ({:0.2f}%)".format(column, get_sign(diff), diff, percent_change)) 
 
 def main():
-    parser = argparse.ArgumentParser(description="Use gadget_compare to compare two runs of intel power gadget.")
-    parser.add_argument("baseline_dir", help="The directory containing reports for the baseline measurments.")
-    parser.add_argument("experiment_dir", help="The directory containing reports for the measurments of the modified code.")
+    parser = argparse.ArgumentParser(description="Use gadget_compare to compare results from discharge.sh.")
+    parser.add_argument("baseline_csv", help="The report containing the baseline measurments.")
+    parser.add_argument("experiment_csv", help="The report containing the measurments of the experiment.")
     args = vars(parser.parse_args())
 
-    baseline = load_dir(args["baseline_dir"])
-    experiment = load_dir(args["experiment_dir"])
-
+    baseline = read_csv(args["baseline_dir"])
+    experiment = read_csv(args["experiment_dir"])
     compare(baseline, experiment)
-
 
 if __name__ == "__main__":
     main()
