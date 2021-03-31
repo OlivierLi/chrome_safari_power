c14666d27d95019b015daf8aa17ef98ece90829d
Olivier Li <OlivierLiDupusi@gmail.com>
2021-03-31 12:40:54 -0400

    Better print, no warning

--- discharge_compare.py
+++ discharge_compare.py
@@ -9,6 +9,7 @@ import os
 import fnmatch
 import shutil
 import argparse
+import pathlib
 
 def read_csv(dir_path):
     df = pd.read_csv(dir_path, engine="python", header=0, skipfooter=0, na_values = [''])
@@ -29,40 +30,44 @@ def clean(df):
     # https://stackoverflow.com/a/24762240
     assert(df.shape[1] == 1)
 
-    df['z_score'] = df.apply(lambda x: (x - x.mean())/x.std())
+    df = df.assign(z_score=df.apply(lambda x: (x - x.mean())/x.std()))
     df = df[abs(df['z_score']) < 3]
 
     cleaned = df.drop(columns=['z_score'])    
 
-    print("Cleaned up :" + str(abs(cleaned.shape[0] - df.shape[0])) + " samples.")
-
     return cleaned
 
+def print_result(name, df, column, ):
+    print(name+":"+column)
+    print("avg : {:0.2f}".format(df[column].mean())) 
+    print("max : {:0.2f}".format(df[column].max())) 
+    print("min : {:0.2f}".format(df[column].min())) 
 
-def compare(baseline, experiment):
+def compare(baseline, baseline_name, experiment, experiment_name):
     assert(baseline.columns.equals(experiment.columns))
 
     # In relation to the size of the baseline dataset.
     acceptable_experiment_size_difference = 0.1
     if abs(baseline.shape[0] - experiment.shape[0]) / baseline.shape[0] > 0.1:
-        print("The two datasets have mismatched sizes, rerun experiments to get comparable data")
+        print("The two datasets have mismatched sizes (after cleaning), rerun experiments to get comparable data")
         # return
 
     for column in baseline.columns:
+        print("---")
+        print_result(baseline_name, baseline, column)
+        print("---")
+        print_result(experiment_name, experiment, column)
+        print("---")
+
         baseline_value = baseline[column].mean()
         experiment_value = experiment[column].mean() 
         diff = experiment_value - baseline_value 
         percent_change = 100*diff/baseline_value 
-
-        print("Baseline avg {}: {:0.2f}".format(column, baseline_value)) 
-        print("Baseline max {}: {:0.2f}".format(column, baseline[column].max())) 
-        print("Baseline min {}: {:0.2f}".format(column, baseline[column].min())) 
-
-        print("Experiment avg {}: {:0.2f}".format(column, experiment_value)) 
-        print("Experiment max {}: {:0.2f}".format(column, experiment[column].max()))
-        print("Experiment min {}: {:0.2f}".format(column, experiment[column].min()))
-
         print("Change in avg {}: {}{:0.2f} ({:0.2f}%)".format(column, get_sign(diff), diff, percent_change)) 
+        print("---")
+
+def get_name(path):
+    return os.path.basename(os.path.dirname(path)) + "-" + pathlib.PurePath(path).stem 
 
 def main():
     parser = argparse.ArgumentParser(description="Use gadget_compare to compare results from discharge.sh.")
@@ -72,7 +77,7 @@ def main():
 
     baseline = read_csv(args["baseline_csv"])
     experiment = read_csv(args["experiment_csv"])
-    compare(baseline, experiment)
+    compare(baseline, get_name(args["baseline_csv"]), experiment, get_name(args["experiment_csv"]), )
 
 if __name__ == "__main__":
     main()
