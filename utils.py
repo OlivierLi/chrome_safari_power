8c73d6b8b740880c55d15708890924f189cdc388
Etienne Pierre-Doray <etipdoray@gmail.com>
2021-04-01 12:30:38 -0400

    Add missing utils

---
+++ utils.py
@@ -0,0 +1,24 @@
+
+
+browsers_definition = {
+  "Chrome": {
+    "executable": "Google Chrome",
+    "identifier": "com.google.Chrome"
+  },
+  "Canary": {
+    "executable": "Google Chrome Canary",
+    "identifier": "com.google.Chrome.canary"
+  },
+  "Chromium": {
+    "executable": "Chromium",
+    "identifier": "org.chromium.Chromium"
+  },
+  "Edge": {
+    "executable": "Microsoft Edge",
+    "identifier": "" # TODO: figure out Edge identifier
+  },
+  "Safari": {
+    "executable": "Safari",
+    "identifier": "com.apple.Safari"
+  }
+}
