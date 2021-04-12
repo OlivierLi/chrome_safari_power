255ac2af7605e6b96d2c62ce62daf87970dd8ffa
Olivier Li <olivierli@google.com>
2021-04-12 09:36:36 -0400

    Start implementing profiling feature

--- utils.py
+++ utils.py
@@ -3,22 +3,27 @@
 browsers_definition = {
   "Chrome": {
     "executable": "Google Chrome",
+    "process_name": "Google Chrome",
     "identifier": "com.google.Chrome"
   },
   "Canary": {
     "executable": "Google Chrome Canary",
+    "process_name": "Google Chrome Canary",
     "identifier": "com.google.Chrome.canary"
   },
   "Chromium": {
-    "executable": "Chromium",
+    "executable": "/Users/olivier/git/chromium/src/out/Release/Chromium.app/Contents/MacOS/Chromium",
+    "process_name": "Chromium",
     "identifier": "org.chromium.Chromium"
   },
   "Edge": {
     "executable": "Microsoft Edge",
+    "process_name": "Microsoft Edge",
     "identifier": "com.microsoft.edgemac"
   },
   "Safari": {
     "executable": "Safari",
+    "process_name": "Safari",
     "identifier": "com.apple.Safari"
   }
 }
