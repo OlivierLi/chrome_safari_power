3a439576dfc9c1f53477dfd5005491c4f5f33b38
Olivier Li <olivierli@google.com>
2021-04-15 12:44:49 -0400

    Not needed

--- clean.sh
+++
@@ -1,7 +0,0 @@
-rm ./driver_scripts/*alligned_timers*
-rm ./driver_scripts/*_idle*
-rm ./driver_scripts/*navigation*
-rm ./driver_scripts/*scroll*
-rm ./driver_scripts/*open_back*
-rm ./driver_scripts/*zero_wind*
-rm ./driver_scripts/*open_back*
