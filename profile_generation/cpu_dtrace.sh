ae2b16fd31b7ec3904707ae367beef7896cd5ba6
Olivier Li <OlivierLiDupuis@gmail.com>
2021-04-16 16:10:19 -0400

    Flamegraph generating scripts!

---
+++ profile_generation/cpu_dtrace.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+set -eux 
+
+if cd Flamegraph; then git pull && cd ..; else git clone https://github.com/brendangregg/FlameGraph; fi
+mkdir -p samples
+cat ../profile/* | ./Flamegraph/stackcollapse.pl > samples/samples.collapsed
+python3 filter.py --mode cpu_time --stack_file samples/samples.collapsed > samples/samples.collapsed.filtered
+cat ./samples/samples.collapsed.filtered | ./Flamegraph/flamegraph.pl -w 3000 > kernel.svg
