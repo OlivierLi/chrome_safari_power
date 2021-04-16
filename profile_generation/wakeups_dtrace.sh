83fd962a8ac3cc4e1ea26f4ef801cb3978935a26
Olivier Li <OlivierLiDupuis@gmail.com>
2021-04-16 16:12:01 -0400

    Don't push flames

--- profile_generation/wakeups_dtrace.sh
+++ profile_generation/wakeups_dtrace.sh
@@ -6,4 +6,4 @@ if cd Flamegraph; then git pull && cd ..; else git clone https://github.com/bren
 mkdir -p samples
 cat ../profile/* | ./Flamegraph/stackcollapse.pl > samples/samples.collapsed
 python3 filter.py --mode wakeups --stack_file samples/samples.collapsed > samples/samples.collapsed.filtered
-cat ./samples/samples.collapsed.filtered | ./Flamegraph/flamegraph.pl --colors wakeup -w 3000 > kernel.svg
+cat ./samples/samples.collapsed.filtered | ./Flamegraph/flamegraph.pl --colors wakeup -w 3000 > ./samples/wakeups_flames.svg
