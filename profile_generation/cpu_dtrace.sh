0785afe2a24cb8d24256aea18ed6b72ac8f22a52
Olivier Li <OlivierLiDupuis@gmail.com>
2021-04-16 19:19:15 -0400

    Add readme, make the scripts runable from root

--- profile_generation/cpu_dtrace.sh
+++ profile_generation/cpu_dtrace.sh
@@ -2,9 +2,11 @@
 
 set -eux 
 
+cd "$(dirname "$0")"
+
 if cd Flamegraph; then git pull && cd ..; else git clone https://github.com/brendangregg/FlameGraph; fi
 rm -rf samples
 mkdir -p samples
-cat ../profile/* | ./Flamegraph/stackcollapse.pl > samples/samples.collapsed
+cat $1/* | ./Flamegraph/stackcollapse.pl > samples/samples.collapsed
 python3 filter.py --mode cpu_time --stack_file samples/samples.collapsed > samples/samples.collapsed.filtered
 cat ./samples/samples.collapsed.filtered | ./Flamegraph/flamegraph.pl -w 3000 > ./samples/cpu_flames.svg
--- profile_generation/wakeups_dtrace.sh
+++ profile_generation/wakeups_dtrace.sh
@@ -2,9 +2,11 @@
 
 set -eux 
 
+cd "$(dirname "$0")"
+
 if cd Flamegraph; then git pull && cd ..; else git clone https://github.com/brendangregg/FlameGraph; fi
 rm -rf samples
 mkdir -p samples
-cat ../profile/* | ./Flamegraph/stackcollapse.pl > samples/samples.collapsed
+cat $1/* | ./Flamegraph/stackcollapse.pl > samples/samples.collapsed
 python3 filter.py --mode wakeups --stack_file samples/samples.collapsed > samples/samples.collapsed.filtered
 cat ./samples/samples.collapsed.filtered | ./Flamegraph/flamegraph.pl --colors wakeup -w 3000 > ./samples/wakeups_flames.svg
